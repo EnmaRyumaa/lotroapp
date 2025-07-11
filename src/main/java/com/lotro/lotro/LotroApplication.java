312216c8ca80b445ad52c1d16771298a7261eae5
EnmaRyumaa <fer.augustofs@gmail.com>
2025-07-10 22:39:06 -0300

    Initial commit

---
+++ src/main/java/com/lotro/lotro/LotroApplication.java
@@ -0,0 +1,13 @@
+package com.lotro.lotro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lotro/lotro/LotroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotro.lotro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
